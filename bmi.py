35bca47257e85779633e1ebcd26638a521453779
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-09-24 18:35:46 +0900

    BMI_onry

--- bmi.py
+++ bmi.py
@@ -1,19 +1,20 @@
 class BMI:
     def __init__(self, height, weight):
-        self.height = height  # インスタンス変数
-        self.weight = weight  # インスタンス変数
+        # self.height = height  # インスタンス変数
+        # self.weight = weight  # インスタンス変数
+        self.value = weight / (height**2)
 
-    def calculate_bmi(self):
-        return self.weight / (self.height**2)
+    # def calculate_bmi(self):
+    # return self.weight / (self.height**2)
 
 
 tanaka_bmi = BMI(height=1.80, weight=67.0)
 sasami_bmi = BMI(height=1.58, weight=80.0)
 
 print("tanaka")
-print(tanaka_bmi.height, tanaka_bmi.weight)
-print(tanaka_bmi.calculate_bmi())
+# print(tanaka_bmi.height, tanaka_bmi.weight)
+print(tanaka_bmi.value)
 
 print("ssami")
-print(sasami_bmi.height, sasami_bmi.weight)
-print(sasami_bmi.calculate_bmi())
+# print(sasami_bmi.height, sasami_bmi.weight)
+print(sasami_bmi.value)
