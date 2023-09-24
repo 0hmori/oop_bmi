17d7d4ff0fedf551f5035a563d2522801ac54703
0hmori <mehowomori.fiofio.cute.38@gmail.com>
2023-09-24 18:52:32 +0900

    bmi_kansei

--- bmi.py
+++ bmi.py
@@ -1,20 +1,15 @@
 class BMI:
     def __init__(self, height, weight):
-        # self.height = height  # インスタンス変数
-        # self.weight = weight  # インスタンス変数
         self.value = weight / (height**2)
 
-    # def calculate_bmi(self):
-    # return self.weight / (self.height**2)
-
+    def __str__(self):
+        return f"{self.value:.2f}"
 
 tanaka_bmi = BMI(height=1.80, weight=67.0)
 sasami_bmi = BMI(height=1.58, weight=80.0)
 
 print("tanaka")
-# print(tanaka_bmi.height, tanaka_bmi.weight)
-print(tanaka_bmi.value)
+print(tanaka_bmi)
 
-print("ssami")
-# print(sasami_bmi.height, sasami_bmi.weight)
-print(sasami_bmi.value)
+print("sasami")
+print(sasami_bmi)
